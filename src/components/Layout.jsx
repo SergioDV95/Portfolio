f1096ef430b117a69c29469dab97b4f1b54ad7b4
SergioDV95 <134448590+SergioDV95@users.noreply.github.com>
2024-04-30 22:56:58 -0400

    Fix

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ import NavBar from "./NavBar";
 export default function Layout() {
    return (
       <>
-         <img className="absolute z-[-1] -rotate-[20deg] max-lg:scale-[2] -left-[65%] top-[1%] lg:-top-[150px] 2xl:-top-[5%]" src={ondas} alt="ondas" />
+         <img className="absolute z-[-1] -rotate-[20deg] max-lg:scale-[2] -left-[55%] top-[1%] lg:-top-[150px] 2xl:-top-[5%]" src={ondas} alt="ondas" />
          <NavBar />
          <Outlet />
       </>
