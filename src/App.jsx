6f16410b09627e614893f8e30579205f57dbe860
SergioDV <134448590+SergioDV95@users.noreply.github.com>
2024-06-14 00:00:19 -0400

    suspense

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useReducer, useEffect, Suspense, lazy } from 'react'
 import { contextReducer } from "./assets/JS/reducers";
 import ContextProps from "./assets/JS/createContext";
-import Layout from "./components/Layout";
-import Home from "./pages/Home"; 
-/* import Loading from "./components/Loading";
+/* import Layout from "./components/Layout";
+import Home from "./pages/Home";  */
+import Loading from "./components/Loading";
 const Layout = lazy(() => import("./components/Layout"));
-const Home = lazy(() => import("./pages/Home")); */
+const Home = lazy(() => import("./pages/Home"));
 
 //const delayImport = (importation, delay) => new Promise(resolve => setTimeout(() => resolve(importation), delay)).then((value) => value);
 
@@ -31,10 +31,8 @@ export default function App() {
 	useEffect(() => {
 		const handleLoad = () => dispatch({ type: 'SET_LOAD' });
 		window.addEventListener('load', handleLoad);
-		window.addEventListener('beforeunload', handleLoad);
       return () => {
 			window.removeEventListener('load', handleLoad);
-			window.removeEventListener('beforeunload', handleLoad);
       };
    }, [context.load]);
 	
@@ -46,13 +44,13 @@ export default function App() {
 	return (
 		<BrowserRouter>
 			<ContextProps.Provider value={{ context, dispatch }}>
-				<Routes>
-					<Route path="/Portfolio/" element={<Layout />}>
-						<Route index element={<Home />} />
-					</Route>
-				</Routes>
-				{/* <Suspense fallback={<Loading />}>
-				</Suspense> */}
+				<Suspense fallback={<Loading />}>
+					<Routes>
+						<Route path="/Portfolio/" element={<Layout />}>
+							<Route index element={<Home />} />
+						</Route>
+					</Routes>
+				</Suspense>
 			</ContextProps.Provider>
 		</BrowserRouter>
 	)
