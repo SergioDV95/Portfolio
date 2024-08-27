0dd85f939c6ec652f305a14954efb8df362ffce1
SergioDV95 <134448590+SergioDV95@users.noreply.github.com>
2024-08-26 23:20:53 -0400

    spring animation

--- src/components/LaptopModel.jsx
+++ src/components/LaptopModel.jsx
@@ -8,7 +8,94 @@ import {
    laptop_screen,
    touchpad,
 } from "../assets/JS/imports";
+import { useSpring, animated } from "@react-spring/three";
 import * as THREE from "three";
+
+const LaptopModel = () => {
+   const laptopRef = useRef();
+   const keyboardRef = useRef();
+   const touchpadRef = useRef();
+
+   const baseTexture = useLoader(TextureLoader, laptop_base);
+   const screenTexture = useLoader(TextureLoader, laptop_screen);
+   const keyboardTexture = useLoader(TextureLoader, keyboard);
+   const touchpadTexture = useLoader(TextureLoader, touchpad);
+
+   const { position } = useSpring({
+      from: { position: [15, 0, -15] }, // Start from the edge of the screen
+      to: { position: [0, 0, 0] }, // Move to the original position
+      config: { mass: 1, tension: 170, friction: 26 },
+   });
+
+   // Adjust texture filtering for better quality
+   [baseTexture, screenTexture, keyboardTexture, touchpadTexture].forEach(
+      (texture) => {
+         texture.minFilter = LinearMipMapLinearFilter;
+         texture.magFilter = LinearFilter;
+         texture.anisotropy = 16; // Increase anisotropy for better texture quality
+      }
+   );
+
+   return (
+      <animated.group
+         ref={laptopRef}
+         onPointerDown={(e) => {
+            e.target.setPointerCapture(e.pointerId);
+         }}
+         onPointerUp={(e) => {
+            e.target.releasePointerCapture(e.pointerId);
+         }}
+         rotation={[0.5, -0.5, 0]}
+         position={position}
+      >
+         {/* Base of the laptop */}
+         <RoundedBox
+            castShadow
+            receiveShadow
+            args={[10, 0.5, 7]}
+            radius={0.1}
+            smoothness={4}
+            position={[0, 0, 0]}
+         >
+            <meshStandardMaterial color="#f2f2f2" />
+         </RoundedBox>
+         {/* Screen of the laptop */}
+         <group position={[0, 0.52, -0.27]} rotation={[-0.3, 0, 0]}>
+            {/* Screen */}
+            <RoundedBox
+               args={[9.9, 7, 0.1]}
+               radius={0.1}
+               smoothness={4}
+               position={[0, 3.75, -3.25]}
+               onUpdate={(self) => adjustUVMapping(self.geometry)}
+            >
+               <meshStandardMaterial map={screenTexture} />
+            </RoundedBox>
+            {/* Screen border */}
+            <RoundedBox
+               castShadow
+               receiveShadow
+               args={[10.01, 7.1, 0.1]}
+               radius={0.1}
+               smoothness={4}
+               position={[0, 3.75, -3.26]}
+            >
+               <meshStandardMaterial color="#f2f2f2" />
+            </RoundedBox>
+         </group>
+         {/* Keyboard */}
+         <mesh ref={keyboardRef} position={[0, 0.201, -1]}>
+            <boxGeometry args={[9.9, 0.1, 4]} />
+            <meshStandardMaterial map={keyboardTexture} />
+         </mesh>
+         {/* Touchpad */}
+         <mesh ref={touchpadRef} position={[0, 0.201, 2]}>
+            <boxGeometry args={[3, 0.1, 2]} />
+            <meshStandardMaterial map={touchpadTexture} />
+         </mesh>
+      </animated.group>
+   );
+};
 
 const adjustUVMapping = (geometry) => {
    if (geometry && geometry.attributes && geometry.attributes.position) {
@@ -42,85 +129,6 @@ const adjustUVMapping = (geometry) => {
 
       geometry.setAttribute("uv", new THREE.BufferAttribute(uv, 2));
    }
-};
-
-const LaptopModel = () => {
-   const laptopRef = useRef();
-   const keyboardRef = useRef();
-   const touchpadRef = useRef();
-
-   const baseTexture = useLoader(TextureLoader, laptop_base);
-   const screenTexture = useLoader(TextureLoader, laptop_screen);
-   const keyboardTexture = useLoader(TextureLoader, keyboard);
-   const touchpadTexture = useLoader(TextureLoader, touchpad);
-
-   // Adjust texture filtering for better quality
-   [baseTexture, screenTexture, keyboardTexture, touchpadTexture].forEach(
-      (texture) => {
-         texture.minFilter = LinearMipMapLinearFilter;
-         texture.magFilter = LinearFilter;
-         texture.anisotropy = 16; // Increase anisotropy for better texture quality
-      }
-   );
-
-   return (
-      <group
-         ref={laptopRef}
-         onPointerDown={(e) => {
-            e.target.setPointerCapture(e.pointerId);
-         }}
-         onPointerUp={(e) => {
-            e.target.releasePointerCapture(e.pointerId);
-         }}
-         rotation={[0.5, -0.5, 0]}
-      >
-         {/* Base of the laptop */}
-         <RoundedBox
-				castShadow
-				receiveShadow
-            args={[10, 0.5, 7]}
-            radius={0.1}
-            smoothness={4}
-            position={[0, 0, 0]}
-         >
-            <meshStandardMaterial color="#f2f2f2" />
-         </RoundedBox>
-         {/* Screen of the laptop */}
-         <group position={[0, 0.52, -0.27]} rotation={[-0.3, 0, 0]}>
-            {/* Screen */}
-            <RoundedBox
-               args={[9.9, 7, 0.1]}
-               radius={0.1}
-               smoothness={4}
-               position={[0, 3.75, -3.25]}
-               onUpdate={(self) => adjustUVMapping(self.geometry)}
-            >
-               <meshStandardMaterial map={screenTexture} />
-            </RoundedBox>
-            {/* Screen border */}
-            <RoundedBox
-					castShadow
-					receiveShadow
-               args={[10.01, 7.1, 0.1]}
-               radius={0.1}
-               smoothness={4}
-               position={[0, 3.75, -3.26]}
-            >
-               <meshStandardMaterial color="#f2f2f2" />
-            </RoundedBox>
-         </group>
-         {/* Keyboard */}
-         <mesh ref={keyboardRef} position={[0, 0.201, -1]}>
-            <boxGeometry args={[9.9, 0.1, 4]} />
-            <meshStandardMaterial map={keyboardTexture} />
-         </mesh>
-         {/* Touchpad */}
-         <mesh ref={touchpadRef} position={[0, 0.201, 2]}>
-            <boxGeometry args={[3, 0.1, 2]} />
-            <meshStandardMaterial map={touchpadTexture} />
-         </mesh>
-      </group>
-   );
 };
 
 export default LaptopModel;
